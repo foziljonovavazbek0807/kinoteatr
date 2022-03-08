c3a918f3ec9e7dde79faff934309d9bdfefeca46
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-08 17:19:23 +0500

    git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import Head from "./Head";
-import Main from "./pages/Main"
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
       <div className="App">
-        <Head />
+        <Head/>
       </div>
-      <Routes>
-        <Route exact path="/"component={<Head />} />
-        <Route path="/main" component={<Main />} />
-      </Routes>
-    </Router>
   );
 }
 
--- src/Head.jsx
+++ src/Head.jsx
@@ -3,21 +3,20 @@ import "./Head.css";
 import MOVIELOGO from "./assets/movie-logo.png";
 import VIDEO from "./assets/clood.mp4";
 import { AiOutlineArrowDown } from "react-icons/ai";
-import { Link, Navigate } from "react-router-dom";
 
 function Head() {
   return (
     <div className="Head__container">
-      <video className="bc_mp4" src={VIDEO} autoPlay="true" />
+      <video className="bc_mp4" src={VIDEO} />
       <div className="text_container">
         <a href="#!" className="logo">
           <img src={MOVIELOGO} alt="movie-logo" />
         </a>
         <p className="und_text">Press the button and enjoy the films!</p>
         <AiOutlineArrowDown className="down_arrow" />
-        <Link to="/main" className="start_btn">
+        <a href="#!" className="start_btn">
           Get start
-        </Link>
+        </a>
       </div>
     </div>
   );
