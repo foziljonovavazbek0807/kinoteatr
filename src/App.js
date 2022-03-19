dd4385824085ee67ca766e901c701d015c52f1f9
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-19 17:16:28 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-// import axios from "axios";
-// import Head from "./Head";
-import Main from "./pages/Main";
-// import Basic from "./pages/Basic";
-// import Search from "./pages/Search";
+import Head from "./Head";
 
 function App() {
   return (
       <div className="App">
-        {/* <Head/> */}
-          
-        <Main/>
+        <Head/>
       </div>
   );
 }
--- src/Head.jsx
+++ src/Head.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Head.css";
-import MOVIELOGO from "./assets/movie-logo.png";
-import VIDEO from "./assets/clood.mp4";
+import MovieLogo from "./assets/movie-logo.png";
+import Video from "./assets/cLoOd.mp4";
 import { AiOutlineArrowDown } from "react-icons/ai";
 
 function Head() {
   return (
     <div className="Head__container">
-      <video className="bc_mp4" src={VIDEO} />
+      <video className="bc_mp4" src={Video} />
       <div className="text_container">
         <a href="#!" className="logo">
-          <img src={MOVIELOGO} alt="movie-logo" />
+          <img src={MovieLogo} alt="movie-logo" />
         </a>
         <p className="und_text">Press the button and enjoy the films!</p>
         <AiOutlineArrowDown className="down_arrow" />
